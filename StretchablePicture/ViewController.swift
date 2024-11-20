086c5dd4d0aff9f085ba0b909baf9d0a6f004436
Roman Krendyasov <krendyasov_rn@mail.ru>
2024-11-20 18:35:35 +0400

    Initial Commit

---
+++ StretchablePicture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StretchablePicture
+//
+//  Created by Роман Крендясов on 20.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
